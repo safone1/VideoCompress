b5cf8a29b294c0b4e66ab2e7c6f3f0b5df188e1f
safone1 <77989182+safone1@users.noreply.github.com>
2021-02-06 16:08:05 +0600

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -6,7 +6,7 @@ from bot.get_cfg import get_config
 
 
 class Localisation:
-    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Compressor BOT 🔥</b>. \n\n<b>Please Sent Me Any Telegram Big Video File I Will Compress It To A Small Video File For You! 🥳</b> \n\n/help for more details... \n\nSupport Group :@I_Am_Only_One_1"
+    START_TEXT = "Hello {}! \n\nThis is <b>Video Compressor BOT 🔥</b> \n\n<b>Please Send Me Any Telegram Big Video File I Will Compress It To A Small Video File For You! 🥳</b> \n\n/help for more details... \n\nDev: @I_Am_Only_One_1 🙂"
    
     ABS_TEXT = " Please don't be selfish."
     
@@ -23,9 +23,9 @@ class Localisation:
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
     
-    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}\n\nBy @I_Am_Only_One_1 🔥"
+    COMPRESS_SUCCESS = "📥 Downloaded in {}\n\n📀 Compressed in {}\n\n📤 Uploaded in {}\n\nBy, @I_Am_Only_One_1 🔥"
 
-    COMPRESS_PROGRESS = "⏳ S1 ETA: {}\n🚀 Progress: {}%"
+    COMPRESS_PROGRESS = "⏳ Time Left: {}\n🚀 Progress: {}%"
 
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
     
@@ -43,11 +43,11 @@ class Localisation:
     
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     
-    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️ Already one Process going on! ⚠️ \n\nCheck Live Status on Updates Channel."
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "⚠️Already Some Process Going On!⚠️ \n\nPlease Come Back After A While. Check Live Status on Updates Channel..."
     
     HELP_MESSAGE = get_config(
         "STRINGS_HELP_MESSAGE",
-        "Hi, I am Video Compressor BOT 🔥\n\n1. Send Me Your Telegram Big Video File \n2. Reply The File - /compress And Persentage \nFor Example:- <code>/compress 50</code> \n\nSupport Group :@I_Am_Only_One_1"
+        "Hi, I am Video Compressor BOT 🔥\n\n1. Send or Forward Me Your Telegram Big Video File \n2. Reply The File - /compress <persentage> \nFor Example: <code>/compress 50</code> \n\nThanks For Using!! 🥰"
     )
     WRONG_MESSAGE = get_config(
         "STRINGS_WRONG_MESSAGE",
